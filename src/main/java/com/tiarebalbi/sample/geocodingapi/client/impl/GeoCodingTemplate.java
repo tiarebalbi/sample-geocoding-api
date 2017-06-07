a8d16f8dc3c64c63d5e9ec044160c8ab5d7726f1
Tiarê Balbi <me@tiarebalbi.com>
2017-06-07 08:04:42 +0100

    Updated google search impl

--- src/main/java/com/tiarebalbi/sample/geocodingapi/client/impl/GeoCodingTemplate.java
+++ src/main/java/com/tiarebalbi/sample/geocodingapi/client/impl/GeoCodingTemplate.java
@@ -2,26 +2,28 @@ package com.tiarebalbi.sample.geocodingapi.client.impl;
 
 import com.tiarebalbi.sample.geocodingapi.client.GeoCodingOperations;
 import com.tiarebalbi.sample.geocodingapi.client.model.GoogleSearchResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class GeoCodingTemplate implements GeoCodingOperations {
 
-//  @Autowired
-//  private RestTemplate restTemplate;
+  private final static String XML_ENDPOINT = "https://maps.googleapis.com/maps/api/geocode/xml?address=";
+
+  @Autowired
+  private RestTemplate restTemplate;
 
   @Override
   public GoogleSearchResponse lookup(String address) {
-    String endpoint = buildEndpoint(address);
-
-    GoogleSearchResponse googleSearchResponse = new GoogleSearchResponse();
-    googleSearchResponse.setFormattedAddress(endpoint);
-    googleSearchResponse.setLatitude(2d);
-    return googleSearchResponse;
+    String endpoint = getEndpoint(address);
+    ResponseEntity<GoogleSearchResponse> response = this.restTemplate.getForEntity(endpoint, GoogleSearchResponse.class);
+    return response.getBody();
   }
 
-  private String buildEndpoint(String address) {
-    return "http://" + address;
+  private String getEndpoint(String address) {
+    return XML_ENDPOINT + address;
   }
 
 }
--- src/main/java/com/tiarebalbi/sample/geocodingapi/client/model/GoogleSearchResponse.java
+++ src/main/java/com/tiarebalbi/sample/geocodingapi/client/model/GoogleSearchResponse.java
@@ -4,4 +4,11 @@ public class GoogleSearchResponse {
 
   private GeocodeResponse GeocodeResponse;
 
+  public com.tiarebalbi.sample.geocodingapi.client.model.GeocodeResponse getGeocodeResponse() {
+    return GeocodeResponse;
+  }
+
+  public void setGeocodeResponse(com.tiarebalbi.sample.geocodingapi.client.model.GeocodeResponse geocodeResponse) {
+    GeocodeResponse = geocodeResponse;
+  }
 }
